08c6680e94281d008ada426ec582d01143cfe090
srgrcp <srgrcp@gmail.com>
2021-06-29 09:08:33 -0500

    Deployment config

--- scully.scully-amplify-test.config.ts
+++ scully.scully-amplify-test.config.ts
@@ -3,6 +3,9 @@ export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "scully-amplify-test",
   outDir: './dist/static',
+  puppeteerLaunchOptions: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
   routes: {
   }
 };
